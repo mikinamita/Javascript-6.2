ee332aceecc6519ec9f1dc43fe5a73aaf3e39c30
mikinamita <mikinamita@gmail.com>
2016-12-04 10:38:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+			
+//Assignment 6.2			
+var no1=5;
+var no2=4;
+var no3=6;
+
+if(no1>no2)
+{
+if(no1>no3)
+{
+	console.log("no1 is greater");
+}
+else{
+	console.log("no3 is greater");
+}
+}
+else{
+	console.log("no2 is greater");
+}
+
+
